16e9acef6c14b0873e8eade902719fced58745bf
jhan-dev <jhan.technology@gmail.com>
2023-04-03 16:31:50 -0700

    User can register login and password

--- app.js
+++ app.js
@@ -47,6 +47,23 @@ app.post("/register", function(req, res){
   })
 });
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne()
+  .then({email: username}, function(foundUser){
+    if(foundUser){
+      if(foundUser.password === password){
+        res.render("secrets");
+      }
+    }
+  })
+  .catch(function(err){
+    res.render(err)
+  })
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
